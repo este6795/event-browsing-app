fe24373aff4859a400bbd4b4c2bbe91a1ae3ab27
OrcBeanFries <kyledean021@gmail.com>
2025-09-16 10:34:26 -0600

    fork - creating user accounts

---
+++ backend/userAccount.py
@@ -0,0 +1,17 @@
+class userAccount:
+    def __init__():
+        # add username
+        # add password
+        # add password to DB
+        # add recovery email
+        # add email to DB
+        pass
+
+    def getPassword():
+        pass
+
+    def setPassword():
+        pass
+
+    def validatePass():
+        pass
