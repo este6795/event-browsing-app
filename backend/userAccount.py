8a92ff76d64cfc950098420de1abf11bd7baea4b
OrcBeanFries <kyledean021@gmail.com>
2025-09-17 21:02:51 -0600

    Update userAccount.py

--- backend/userAccount.py
+++ backend/userAccount.py
@@ -1,17 +1,46 @@
+#import SQLalchemy
+
+# NEED TO ASK HOW MUCH OF THIS LOGIC IS ALREADY GOING TO BE HANDLED BY THE FRONT END
 class userAccount:
-    def __init__():
-        # add username
-        # add password
+    def __init__(self, user = None, userPass, email):
+        #username = verifyUsernameUnique(user)
+        
+        password = userPass
         # add password to DB
-        # add recovery email
+        
+
+        #VERIFY EMAIL IS REAL
+        #recoveryEmail = email
         # add email to DB
         pass
 
-    def getPassword():
+    def verifyUsernameUnique(self, username):
+        # VERIFY USERNAME IS UNIQUE
+        # user inputs their email
+        # call to database to check uniqueness
+        # while loop until they submit a valid one
+        
+        return
+    
+    def verifyEmailValid(self, email):
+        #while loop until they submit a valid email
         pass
 
-    def setPassword():
+    def changePass(self, newPass):
+        #assuming that it has already been verified that this is a valid password change
+        self.password = newPass
+
+    def changeEmail(self, newEmail):
+        self.email = verifyEmailValid(newEmail)
+
+
+    def getEncryptedPassHash(self):
+        #used when logging in, to check user input password attempt against the stored password
         pass
 
-    def validatePass():
+    def attemptingLogin(self):
+        #for loop, only given three tries to submit a valid password
+
+
         pass
+
